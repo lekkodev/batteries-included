7e7e655537228e0d4a7f1f605d11c99301150f2f
Jonathan Dobbie <jonathan@lekko.com>
2024-03-06 14:12:44 -0800

    transform build

--- alert-banner-example/vite.config.ts
+++ alert-banner-example/vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { spawnSync } from "child_process";
 
+function lekkoPlugin() {
+  const fileRegex = /lekko\/[a-z\-]+.ts$/
+  return {
+    name: 'transform-lekko-files',
+    enforce: 'pre',
+
+    transform(src, id) {
+      if (fileRegex.test(id)) {
+        const result = spawnSync("npx", ["print-transformed", "-f", id]);
+        return {
+          code: result.stdout.toString()
+        }
+      }
+    },
+  }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [lekkoPlugin(), react()],
   build: {
     minify: false
   }
