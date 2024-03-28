ce1e9911c7aafc97e6e4e17b5198c3f71af5a727
David Kang <david@lekko.com>
2024-03-27 18:14:00 -0700

    Before state

--- alert-banner-example-nextjs-app/app/layout.tsx
+++ alert-banner-example-nextjs-app/app/layout.tsx
@@ -1,9 +1,6 @@
-import "@lekko/react-banner/styles.css";
 import "./index.css";
 import Link from "next/link";
 import type { Metadata } from "next";
-import { getBannerConfig, getBannerStyles } from "@/lekko/plugins";
-import BannerProvider from "@/components/banner";
 
 // TODO: icon
 export const metadata: Metadata = {
@@ -18,41 +15,39 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <BannerProvider>
-          <div id="root">
-            <div id="sidebar">
-              <h1>
-                <Link href="/">Lekko Alert Banner Example</Link>
-              </h1>
-              <nav>
-                <h2>Pages</h2>
-                <ul>
-                  <li>
-                    <Link href="/home">Home</Link>
-                  </li>
-                  <li>
-                    <Link href="/beta">Beta ✨</Link>
-                  </li>
-                </ul>
-              </nav>
-              <div>
-                <h2>Integrations</h2>
-                <ul>
-                  <li>
-                    <Link href="/integrations/github">GitHub</Link>
-                  </li>
-                  <li>
-                    <Link href="/integrations/slack">Slack</Link>
-                  </li>
-                  <li>
-                    <Link href="/integrations/openai">OpenAI</Link>
-                  </li>
-                </ul>
-              </div>
+        <div id="root">
+          <div id="sidebar">
+            <h1>
+              <Link href="/">Lekko Alert Banner Example</Link>
+            </h1>
+            <nav>
+              <h2>Pages</h2>
+              <ul>
+                <li>
+                  <Link href="/home">Home</Link>
+                </li>
+                <li>
+                  <Link href="/beta">Beta ✨</Link>
+                </li>
+              </ul>
+            </nav>
+            <div>
+              <h2>Integrations</h2>
+              <ul>
+                <li>
+                  <Link href="/integrations/github">GitHub</Link>
+                </li>
+                <li>
+                  <Link href="/integrations/slack">Slack</Link>
+                </li>
+                <li>
+                  <Link href="/integrations/openai">OpenAI</Link>
+                </li>
+              </ul>
             </div>
-            <div id="page">{children}</div>
           </div>
-        </BannerProvider>
+          <div id="page">{children}</div>
+        </div>
       </body>
     </html>
   );
--- alert-banner-example-nextjs-app/next.config.mjs
+++ alert-banner-example-nextjs-app/next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config, { isServer }) {
-    if (!isServer && process.env.NODE_ENV === "production") {
-      config.module.rules.push({
-        test: /lekko\/.*\.ts$/,
-        loader: "@lekko/webpack-loader",
-      });
-    }
-    return config;
-  },
   images: {
     remotePatterns: [
       {
